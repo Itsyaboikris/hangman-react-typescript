c81e068bcc2b8469b8db04fc81293d39993b4fdb
itsyaboikris <kristoff_riley@outlook.com>
2022-11-20 18:04:28 -0400

    hangman pole

--- src/HangmanDrawing.tsx
+++ src/HangmanDrawing.tsx
@@ -1,7 +1,16 @@
 export function HangmanDrawing() {
+
 	return (
-		<div>
-			Drawing
+		<div style={{ position: "relative" }}>
+
+			{/* top hook */}
+			<div style={{ height: "50px", width:"10px", background:"black", position:"absolute", top: 0, right: 0 }} />
+			{/* top horizontal bar */}			
+			<div style={{ height: "10px", width:"200px", background:"black", marginLeft:"120px" }} />
+			{/* vertical bar */}
+			<div style={{ height: "400px", width:"10px", background:"black", marginLeft:"120px" }} />
+			{/* bottom bar */}
+			<div style={{ height: "10px", width: "250px", background: "black"  }} />
 		</div>
 	)
 }
