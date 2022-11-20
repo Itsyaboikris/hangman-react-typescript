30198cf830852b9477728058ba16b48e2fdac135
itsyaboikris <kristoff_riley@outlook.com>
2022-11-20 18:18:05 -0400

    body parts

--- src/HangmanDrawing.tsx
+++ src/HangmanDrawing.tsx
@@ -1,7 +1,89 @@
+const HEAD = (
+	<div style={{ 
+		width: "50px",
+		height: "50px",
+		borderRadius: "100%",
+		border: "10px solid black",
+		position: "absolute",
+		top: "50px",
+		right: "-30px"
+	 }} />
+)
+
+const BODY = (
+	<div style={{ 
+		width: "10px",
+		height: "100px",
+		background: "black",
+		position: "absolute",
+		top: "120px",
+		right: 0
+	 }} />
+)
+
+const RIGHTARM = (
+	<div style={{ 
+		width: "100px",
+		height: "10px",
+		background: "black",
+		position: "absolute",
+		top: "150px",
+		right: "-100px",
+		rotate: "-30deg",
+		transformOrigin: "left bottom"
+	 }} />
+)
+
+const LEFTARM = (
+	<div style={{ 
+		width: "100px",
+		height: "10px",
+		background: "black",
+		position: "absolute",
+		top: "150px",
+		right: "10px",
+		rotate: "30deg",
+		transformOrigin: "right bottom"
+	 }} />
+)
+
+const RIGHTLEG = (
+	<div style={{ 
+		width: "100px",
+		height: "10px",
+		background: "black",
+		position: "absolute",
+		top: "210px",
+		right: "-90px",
+		rotate: "60deg",
+		transformOrigin: "left bottom"
+	 }} />
+)
+
+const LEFTLEG = (
+	<div style={{ 
+		width: "100px",
+		height: "10px",
+		background: "black",
+		position: "absolute",
+		top: "210px",
+		right: "0",
+		rotate: "-60deg",
+		transformOrigin: "right bottom"
+	 }} />
+)
+
 export function HangmanDrawing() {
 
 	return (
 		<div style={{ position: "relative" }}>
+
+			{HEAD}
+			{BODY}
+			{RIGHTARM}
+			{LEFTARM}
+			{RIGHTLEG}
+			{LEFTLEG}
 
 			{/* top hook */}
 			<div style={{ height: "50px", width:"10px", background:"black", position:"absolute", top: 0, right: 0 }} />
