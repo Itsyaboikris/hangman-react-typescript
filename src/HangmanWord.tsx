b45b8c7ba56262e3854ee072eb08e381e2a3470f
itsyaboikris <kristoff_riley@outlook.com>
2022-11-20 18:29:33 -0400

    hangman word layout

--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -1,7 +1,26 @@
 export function HangmanWord() {
+
+	const word = "hello"
+	const guessedLetters =["e"]
+
 	return (
-		<div>
-			Word
+		<div style ={{ 
+			display: "flex",
+			gap: ".25em",
+			fontSize: "6rem",
+			fontWeight: "bold",
+			textTransform: "uppercase",
+			fontFamily: "monospace"
+		 }}>
+			{word.split("").map((letter, index) => (
+				<span style={{ borderBottom: ".1em solid black" }} key={index}>
+					<span style={{ 
+						visibility: guessedLetters.includes(letter) ? "visible" : "hidden"
+					 }}> 
+						{letter}
+					</span>
+				</span>
+			))}
 		</div>
 	)
 }
